20d493265103b105d860329e2e7ff333cc849373
TheMaster1127 <Dzemilefere@gmail.com>
2025-06-24 01:00:14 +0300

    3 ide

---
+++ Desktop/test.js
@@ -0,0 +1,15 @@
+
+
+function print(value) {
+    console.log(value)
+}
+
+
+async function main() {
+    for (let A_Index1 = 0; A_Index1 < 5 + 0; A_Index1++) {
+        print(A_Index1);
+    }
+    
+
+}
+main();
---
+++ Desktop/test.py
@@ -0,0 +1,8 @@
+
+
+# used the print func
+
+
+for A_Index1 in range(0, 13 + 0):
+    print(A_Index1)
+
